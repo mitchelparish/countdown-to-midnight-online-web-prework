7424e4f97a22eee5c72960b79460e2855e2d962a
Mitchel Parish <mitchelparish@gmail.com>
2018-12-27 21:02:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,13 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 
 end
+
+def countdown_with_sleep(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    sleep(1_secs)
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+
+end
